3d4ca6ff7e25d469db534bdf49886c85f8199d4e
jshuckbot <gorka_ivan@mail.ru>
2023-07-01 14:43:40 +0700

    fix in settings

--- config/settings.py
+++ config/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accountapp.apps.AccountappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accountapp.apps.AccountappConfig',
 ]
 
 MIDDLEWARE = [
