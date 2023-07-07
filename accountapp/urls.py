4c67abf31413f5ece5817e78735734eaa5dda315
jshuckbot <gorka_ivan@mail.ru>
2023-07-07 18:55:11 +0700

    fix urls accountapp

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,5 +1,4 @@
-from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import include, path
 
 from accountapp import views
 
@@ -9,17 +8,19 @@ urlpatterns = [
     # предыдущий  url входа
     # path("login/", views.user_login, name="login"),
     # url-адреса входа выхода
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    # url для смены пароля
-    path("password-change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
-    path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
-    # url для сброса пароля
-    path("password-reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
-    path("password-reset/done", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path(
-        "password-reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"
-    ),
-    path("password-reset/complite/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    # path("login/", auth_views.LoginView.as_view(), name="login"),
+    # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # # url для смены пароля
+    # path("password-change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
+    # path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    # # url для сброса пароля
+    # path("password-reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    # path("password-reset/done", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    # path(
+    #     "password-reset/<uidb64>/<token>/",
+    #     auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"
+    # ),
+    # path("password-reset/complite/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path("", include("django.contrib.auth.urls")),
     path("", views.dashboard, name="dashboard"),
 ]
