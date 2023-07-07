967f489626a2c184cd6ebfe8fe5fb43e35919cd4
jshuckbot <gorka_ivan@mail.ru>
2023-07-07 13:34:34 +0700

    add view change password

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from accountapp import views
 from accountapp.apps import AccountappConfig
@@ -12,5 +12,12 @@ urlpatterns = [
     # url-адреса входа выхода
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # url для смены пароля
+    path(
+        "password-change/",
+        auth_views.PasswordChangeView.as_view(success_url=reverse_lazy("accountapp:password_change_done")),
+        name="password_change",
+    ),
+    path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
     path("", views.dashboard, name="dashboard"),
 ]
