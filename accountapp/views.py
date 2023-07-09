9c6005d6c1248995bde959be567f2245665a193f
jshuckbot <gorka_ivan@mail.ru>
2023-07-09 16:34:17 +0700

    extend model user

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from accountapp.forms import LoginForm, UserRegistrationForm
+from accountapp.forms import LoginForm, ProfileEditForm, UserEditForm, UserRegistrationForm
+from accountapp.models import Profile
 
 
 def user_login(request):
@@ -38,8 +39,24 @@ def register(request):
             new_user.set_password(user_form.cleaned_data["password"])
             # Сохраняем объект пользователя
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request, "accountapp/register_done.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm()
 
     return render(request, "accountapp/register.html", {"user_form": user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(request, "accountapp/edit.html", {"user_form": user_form, "profile_form": profile_form})
