2c63ee948b4e9205c07e9926b5366224c7cb93ac
jshuckbot <gorka_ivan@mail.ru>
2023-07-22 16:03:30 +0700

    added view detail about image

--- imagesapp/urls.py
+++ imagesapp/urls.py
@@ -4,4 +4,7 @@ from imagesapp import views
 
 app_name = "imagesapp"
 
-urlpatterns = [path("create/", views.image_create, name="create")]
+urlpatterns = [
+    path("create/", views.image_create, name="create"),
+    path("detail/<int:id>/<slug:slug>/", views.image_detail, name="detail"),
+]
--- imagesapp/views.py
+++ imagesapp/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from imagesapp.forms import ImageCreateForm
+from imagesapp.models import Image
 
 
 @login_required
@@ -26,3 +27,8 @@ def image_create(request):
         form = ImageCreateForm(data=request.GET)
 
     return render(request, "imagesapp/image/create.html", {"section": "images", "form": form})
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request, "imagesapp/image/detail.html", {"section": "images", "image": image})
