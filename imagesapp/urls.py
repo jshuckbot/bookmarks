36c2f706f5455eea4168c87ad14664d4956bc8b5
jshuckbot <gorka_ivan@mail.ru>
2023-07-23 07:01:42 +0700

    added view image_like

--- imagesapp/urls.py
+++ imagesapp/urls.py
@@ -7,4 +7,5 @@ app_name = "imagesapp"
 urlpatterns = [
     path("create/", views.image_create, name="create"),
     path("detail/<int:id>/<slug:slug>/", views.image_detail, name="detail"),
+    path("like/", views.image_like, name="like"),
 ]
--- imagesapp/views.py
+++ imagesapp/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
 
 from imagesapp.forms import ImageCreateForm
 from imagesapp.models import Image
@@ -32,3 +34,21 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, "imagesapp/image/detail.html", {"section": "images", "image": image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == "like":
+                image.user_like.add(request.user)
+            else:
+                image.user_like.remove(request.user)
+            return JsonResponse({"status": "ok"})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({"status": "error"})
