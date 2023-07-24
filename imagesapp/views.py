63d1a9b835057411e314040acd2c03b43cf52200
jshuckbot <gorka_ivan@mail.ru>
2023-07-24 09:20:33 +0700

    added inf scroll

--- imagesapp/views.py
+++ imagesapp/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
 
@@ -52,3 +53,24 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse({"status": "error"})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get("page")
+    images_only = request.GET.get("images_only")
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse("")
+        images = paginator.page(paginator.num_pages)
+
+    if images_only:
+        return render(request, "imagesapp/image/list_images.html", {"section": "images", "images": images})
+
+    return render(request, "imagesapp/image/list.html", {"section": "images", "images": images})
