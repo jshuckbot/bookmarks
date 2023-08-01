cd1ccf82388c4e7223c5d6e619dbb11c975b400b
jshuckbot <gorka_ivan@mail.ru>
2023-08-01 10:00:37 +0700

    added in view by accountapp user_follow

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,11 +2,12 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 
 from accountapp.forms import LoginForm, ProfileEditForm, UserEditForm, UserRegistrationForm
-from accountapp.models import Profile
+from accountapp.models import Contact, Profile
 
 
 def user_login(request):
@@ -77,3 +78,23 @@ def user_list(request):
 def user_detail(request, username):
     user = get_object_or_404(User, username=username, is_active=True)
     return render(request, "accountapp/user/detail.html", {"section": "people", "user": user})
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == "follow":
+                Contact.objects.get_or_create(user_from=request.user, user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({"status": "ok"})
+
+        except User.DoesNotExist:
+            return JsonResponse({"status": "error"})
+
+    return JsonResponse({"status": "error"})
